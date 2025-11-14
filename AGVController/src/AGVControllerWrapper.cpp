b8035dde1d38e5e88d223d02d598167eb7a7c074
Muvtons <design5muvtons@gmail.com>
2025-11-14 12:50:13 +0530

    Delete AGVController directory

--- AGVController/src/AGVControllerWrapper.cpp
+++
@@ -1,6 +0,0 @@
-#include "AGVControllerWrapper.h"
-
-void setupAGVController(const char* adminUser, const char* adminPass) {
-    AGV.begin(adminUser, adminPass);
-}
-
--- AGVController/src/AGVControllerWrapper.h
+++
@@ -1,9 +0,0 @@
-#ifndef AGVCONTROLLER_WRAPPER_H
-#define AGVCONTROLLER_WRAPPER_H
-
-#include "AGVController.h"
-
-// Convenience function for single-line usage
-void setupAGVController(const char* adminUser = "admin", const char* adminPass = "admin123");
-
-#endif
